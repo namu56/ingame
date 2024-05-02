e172821e1ce55ef1c422e98950575dd99d34b469
aquaman122 <swj981128@gmail.com>
2024-05-02 14:10:43 +0900

    test 용도로 작성한 코드 삭제

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,17 +1,9 @@
 import styled from 'styled-components';
 import React from 'react';
-// import SubBox from '@/components/SubBox';
-import MainBox from '@/components/MainBox';
-import RankingBox from '@/components/RankingBox';
-import SubBox from '@/components/SubBox';
 
 const MainPage = () => {
   return (
-    <MainPageStyle>
-      <MainBox /> 
-      {/* <RankingBox /> */}
-      {/* <SubBox /> */}
-    </MainPageStyle>
+    <div>MainPage</div>
   );
 };
 
