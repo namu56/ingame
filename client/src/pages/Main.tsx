cd530a8a299edc9bc467787833bb0a92d2ed10cf
aquaman122 <swj981128@gmail.com>
2024-05-02 20:13:08 +0900

    feat: markup 기본적인것 구현

---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Ranking from './Ranking';
+
+const Main = () => {
+  return (
+    <MainStyle>
+      <Ranking />
+    </MainStyle>
+  );
+};
+
+const MainStyle = styled.div``;
+
+export default Main;
--- client/src/pages/Ranking.tsx
+++ client/src/pages/Ranking.tsx
@@ -1,13 +1,51 @@
 import styled from 'styled-components';
+import { GiHamburgerMenu } from "react-icons/gi";
+import RankingBox from '@/components/RankingBox';
+import Top from '@/components/Top';
+import tropy from '@/assets/images/tropy.png';
+
 
 const Ranking = () => {
   return (
     <RankingStyle>
-      <h1>Ranking</h1>
+      <header className='hamberger'><GiHamburgerMenu /></header>
+      <div className='rankTitleContainer'>
+        <img src={tropy} alt="tropy" />
+        <p>랭킹 순위</p>
+      </div>
+      <div>
+        <RankingBox />
+      </div>
+      <div className='topBtn'><Top /></div>
     </RankingStyle>
   );
 };
 
-const RankingStyle = styled.div``;
+const RankingStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  background: ${({ theme }) => theme.color.white};
+
+  .hamberger {
+    display: flex;
+    justify-content: flex-end;
+    font-size: ${({ theme }) => theme.font.large};
+  }
+
+  .rankTitleContainer {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 15px;
+    padding-bottom: 10px;
+    font-size: ${({ theme }) => theme.font.medium};
+  }
+
+
+  .topBtn {
+    
+  }
+`;
 
 export default Ranking;
