06105f3f852b8af962afe5bd366f6c218c4200b4
aquaman122 <swj981128@gmail.com>
2024-05-04 20:36:39 +0900

    feat: 공통 컴포넌트 inputBox 2개 생성

---
+++ client/src/components/ProfileIntroInputBox.tsx
@@ -0,0 +1,51 @@
+import React, { useState, ForwardedRef } from 'react';
+import styled from 'styled-components';
+
+interface ProfileIntroInputBoxProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  placeholder?: string;
+}
+
+const ProfileIntroInputBox = React.forwardRef(
+  (
+    { placeholder, ...props }: ProfileIntroInputBoxProps,
+    ref: ForwardedRef<HTMLTextAreaElement>
+  ) => {
+    const [profileIntro, setprofileIntro] = useState(placeholder);
+
+  const onChangeHandler = (event: any) => {
+    event.preventDefault();
+    setprofileIntro(event.target.value);
+  }
+
+    return (
+      <ProfileIntroInputBoxLayoutStyle>
+        <ProfileIntroInputBoxStyle placeholder={placeholder} ref={ref} {...props} value={profileIntro} onChange={onChangeHandler} />
+      </ProfileIntroInputBoxLayoutStyle>
+    );
+  }
+);
+
+const ProfileIntroInputBoxLayoutStyle = styled.div`
+  display: flex;
+  align-items: flex-start;
+
+  width: 17.6rem;
+  height: 216px;
+  background: ${({ theme }) => theme.color.white};
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+`;
+
+const ProfileIntroInputBoxStyle = styled.textarea`
+  width: 100%;
+  height: 100%;
+  padding: 0.5rem 0.5rem;
+  border: none;
+  background: none;
+  font-size: ${({ theme }) => theme.font.xsmall};
+  color: ${({ theme }) => theme.color.grayDark};
+  resize: none;
+`;
+
+export default ProfileIntroInputBox;
---
+++ client/src/components/QuestInputBox.tsx
@@ -0,0 +1,51 @@
+import React, { useState, ForwardedRef } from 'react';
+import styled from 'styled-components';
+
+interface QuestInputBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+  inputType?: "text" | "number";
+}
+
+const QuestInputBox = React.forwardRef(
+  (
+    { placeholder, inputType, ...props }: QuestInputBoxProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const [questTitle, setQuestTitle] = useState(placeholder);
+
+  const onChangeHandler = (event: any) => {
+    event.preventDefault();
+    setQuestTitle(event.target.value);
+  }
+
+    return (
+      <QuestInputBoxLayoutStyle>
+        <QuestInputBoxStyle placeholder={placeholder} ref={ref} {...props} value={questTitle} onChange={onChangeHandler} />
+      </QuestInputBoxLayoutStyle>
+    );
+  }
+);
+
+const QuestInputBoxLayoutStyle = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 17.6rem;
+  height: 36px;
+  background: ${({ theme }) => theme.color.white};
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+`;
+
+const QuestInputBoxStyle = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 0 0.5rem;
+  border: none;
+  background: none;
+  font-size: ${({ theme }) => theme.font.xsmall};
+  color: ${({ theme }) => theme.color.grayDark};
+`;
+
+export default QuestInputBox;
