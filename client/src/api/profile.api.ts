9963143f15b8269ee8ee6c9bf0cb95d6c08ebb7d
aquaman122 <swj981128@gmail.com>
2024-05-07 14:38:45 +0900

    chore: user.api에 넣었습니다

--- client/src/api/profile.api.ts
+++
@@ -1,8 +0,0 @@
-import { API_END_POINT } from '@/constant/api';
-import { UserProfile } from '@/models/user.model';
-import { httpClient } from '@/utils/axios';
-
-export const patchProfile = async (data: UserProfile) => {
-  const response = await httpClient.patch(API_END_POINT.PROFILE, data);
-  return response.data;
-};
