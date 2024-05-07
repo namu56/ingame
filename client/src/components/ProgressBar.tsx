1491dd936f01745d332782b8b10db3e44ea0b52e
Sunghyun627 <kksscc4@gmail.com>
2024-05-07 13:13:12 +0900

    feat: 포인트(경험치) progress Bar 구현

---
+++ client/src/components/ProgressBar.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { Progress } from 'antd';
+
+interface ProgressBarProps {
+  point: number;
+}
+
+const calcProgressPercent = (point: number) => Math.floor(((point % 1024) / 1024) * 100);
+
+const ProgressBar = ({ point }: ProgressBarProps) => {
+  return (
+    <ProgressBarStyle>
+      <Progress
+        percent={calcProgressPercent(point)}
+        size={['100%', 28]}
+        strokeColor="#0ACF83"
+        trailColor="#E2F8F1"
+      />
+    </ProgressBarStyle>
+  );
+};
+
+const ProgressBarStyle = styled.div`
+  width: 100%;
+`;
+
+export default ProgressBar;
