4bb08d1a7453b0c02bcd48e603480b9c74b3c020
aquaman122 <swj981128@gmail.com>
2024-05-12 11:56:00 +0900

    chore: combine quests, quest

--- client/src/api/quests.api.ts
+++ client/src/api/quests.api.ts
@@ -1,10 +1,20 @@
 import { SubQuestModifyProps } from '@/components/modals/SubQuestModal';
 import { API_END_POINT } from '@/constant/api';
-import { SubQuest } from '@/models/quest.model';
+import { Quest, SubQuest } from '@/models/quest.model';
 import { httpClient } from '@/utils/axios';
 
 interface GetSubQuestParam {
   date: string;
+}
+
+export const createMainQuest = async (data: Quest) => {
+  const response = await httpClient.post(API_END_POINT.CREATE_QUEST, { ...data });
+  return response.data;
+};
+
+export const modiMainQuest = async (data: Quest) => {
+  const response = await httpClient.patch(API_END_POINT.PATCH_QUEST + `/${data.id}`, { ...data });
+  return response.data;
 }
 
 export const getSubQuest = async (param: GetSubQuestParam) => {
--- client/src/mocks/handlers/quests.ts
+++ client/src/mocks/handlers/quests.ts
@@ -1,7 +1,22 @@
 import { http, HttpResponse } from 'msw';
 import { API_END_POINT } from '@/constant/api';
 import { mockSubQuest } from '../data/mockSubQuest';
-import { SubQuest } from '@/models/quest.model';
+import { Quest, SubQuest } from '@/models/quest.model';
+import { mockMainQuests } from '../data/mockMainQuest';
+
+export const createMainQuest = http.post(`${API_END_POINT.CREATE_QUEST}`, async () => {
+  return new HttpResponse(JSON.stringify({ status: 'created', message: 'success' }));
+});
+
+export const modiMainQuest = http.patch(`${API_END_POINT.PATCH_QUEST}/:id`, async ({ request, params}) => {
+  const data = (await request.json()) as Omit<Quest, 'status'>;
+  mockMainQuests.map((quest) => {
+    if (quest.id === Number(params.id)) {
+      quest.title = data.title;
+    }
+  })
+  return new HttpResponse(JSON.stringify(mockMainQuests), { status: 200, statusText: `success` });
+});
 
 export const getSubQuest = http.get(`${API_END_POINT['SUB_QUEST']}`, async (date) => {
   return new HttpResponse(JSON.stringify(mockSubQuest), { status: 200, statusText: `success` });
