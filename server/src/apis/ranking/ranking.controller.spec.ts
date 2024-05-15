158521fafda0335458737ac34a31ff19d7f68cb3
namu56 <swi920224@gmail.com>
2024-05-15 17:25:50 +0900

    test: 랭킹 조회 테스트 코드 작성

--- server/src/apis/ranking/ranking.controller.spec.ts
+++ server/src/apis/ranking/ranking.controller.spec.ts
@@ -1,20 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { RankingController } from './ranking.controller';
 import { RankingService } from './ranking.service';
+import * as request from 'supertest'; // supertest 임포트
 
 describe('RankingController', () => {
-  let controller: RankingController;
+  let app: INestApplication;
+  let rankingService: RankingService;
+
+  const mockRankingService = {
+    getRanking: jest.fn().mockResolvedValue([
+      { id: 1, nickname: 'user1', point: 100, rank: 1 },
+      { id: 2, nickname: 'user2', point: 90, rank: 2 },
+    ]),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RankingController],
-      providers: [RankingService],
+      providers: [
+        {
+          provide: RankingService,
+          useValue: mockRankingService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<RankingController>(RankingController);
+    app = module.createNestApplication();
+    await app.init();
+    rankingService = module.get<RankingService>(RankingService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('사용자 랭킹을 배열 형태로 리턴하나?', async () => {
+    const response = await request(app.getHttpServer()).get('/ranking').expect(HttpStatus.OK); // supertest를 사용하여 HTTP GET 요청 보내기
+
+    expect(response.body).toEqual([
+      { id: 1, nickname: 'user1', point: 100, rank: 1 },
+      { id: 2, nickname: 'user2', point: 90, rank: 2 },
+    ]);
+    expect(rankingService.getRanking).toHaveBeenCalled();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
