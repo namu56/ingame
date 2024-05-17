e088da1769cef0a2071a25f64cfa2dbf9be9acaa
Sunghyun627 <kksscc4@gmail.com>
2024-05-17 15:07:59 +0900

    style: 에러 페이지 레이아웃 구현

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,13 +1,47 @@
 import styled from 'styled-components';
+import { MdErrorOutline } from 'react-icons/md';
+import Button from '@/components/Button';
 
 const Error = () => {
   return (
     <ErrorStyle>
-      <h1>Error</h1>
+      <ErrorContentStyle>
+        <MdErrorOutline size="60" />
+        <p className="error__text">잘못된 접근입니다.</p>
+      </ErrorContentStyle>
+      <Button size="large" color="blue">
+        메인페이지로 이동하기
+      </Button>
     </ErrorStyle>
   );
 };
 
-const ErrorStyle = styled.div``;
+const ErrorStyle = styled.div`
+  position: absolute;
+  top: 30%;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  gap: 30px;
+`;
+
+const ErrorContentStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+
+  .error__text {
+    font-size: ${({ theme }) => theme.font.xlarge};
+  }
+
+  svg {
+    color: ${({ theme }) => theme.color.coral};
+  }
+`;
 
 export default Error;
