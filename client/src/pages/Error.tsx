55e1c29d46e538ca37f9a2add5167a487e2c3bf3
Sunghyun627 <kksscc4@gmail.com>
2024-05-17 15:14:10 +0900

    feat: 메인페이지 또는 로그인페이지로 navigate 시켜주는 기능 구현

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,16 +1,27 @@
 import styled from 'styled-components';
 import { MdErrorOutline } from 'react-icons/md';
 import Button from '@/components/Button';
+import { useSelector } from 'react-redux';
+import { isLoggedIn } from '@/store/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { ROUTERS } from '@/constant/route';
 
 const Error = () => {
+  const isLogInUser = useSelector(isLoggedIn);
+  const navigate = useNavigate();
+
   return (
     <ErrorStyle>
       <ErrorContentStyle>
         <MdErrorOutline size="60" />
         <p className="error__text">잘못된 접근입니다.</p>
       </ErrorContentStyle>
-      <Button size="large" color="blue">
-        메인페이지로 이동하기
+      <Button
+        size="large"
+        color="blue"
+        onClick={() => navigate(`${isLogInUser ? ROUTERS.MAIN : ROUTERS.AUTH.LOGIN}`)}
+      >
+        {isLogInUser ? `메인페이지로 이동하기` : '로그인 페이지로 이동하기'}
       </Button>
     </ErrorStyle>
   );
