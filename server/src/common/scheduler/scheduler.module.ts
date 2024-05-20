99e079b8d8e93576f2c4c55633d6987e8f7f7538
polaris9017 <moonrise917@gmail.com>
2024-05-20 18:32:08 +0900

    fix: 포인트 업데이트를 위한 포인트 모듈 import 및 컨트롤러 분리

--- server/src/common/scheduler/scheduler.module.ts
+++ server/src/common/scheduler/scheduler.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quest } from '../../apis/quests/entities/quest.entity';
 import { SchedulerService } from './scheduler.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { SchedulerController } from './scheduler.controller';
+import { PointModule } from '../../apis/point/point.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quest]), ScheduleModule.forRoot()],
+  imports: [TypeOrmModule.forFeature([Quest]), ScheduleModule.forRoot(), PointModule],
   providers: [SchedulerService],
+  controllers: [SchedulerController],
   exports: [SchedulerService],
 })
 export class SchedulerModule {}
