e970b3a5f339f6bd2e7e1361ac3aaf01518f25f9
namu56 <swi920224@gmail.com>
2024-06-23 09:58:05 +0900

    refactor: quest entity 수정

--- server/src/entities/quest/quest.entity.ts
+++ server/src/entities/quest/quest.entity.ts
@@ -1,49 +1,64 @@
-import { BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from '../user/user.entity';
 import { SideQuest } from '../side-quest/side-quest.entity';
 import { Difficulty, isHidden, Mode, Status } from '../../common/types/quest/quest.type';
+import { BaseTimeEntity } from 'src/core/database/typeorm/base-time.entity';
 
 @Entity('quest')
-export class Quest extends BaseEntity {
+export class Quest extends BaseTimeEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('int', { nullable: false })
+  @Column({ type: 'int', nullable: false })
   userId: number;
 
   @Column({ type: 'varchar', length: 50, nullable: false })
   title: string;
 
-  @Column({ type: 'enum', name: 'difficulty', enum: Difficulty })
-  difficulty!: Difficulty;
+  @Column({ type: 'enum', enum: Difficulty, nullable: false })
+  difficulty: Difficulty;
 
-  @Column({ type: 'enum', name: 'mode', enum: Mode })
-  mode!: Mode;
+  @Column({ type: 'enum', enum: Mode, nullable: false })
+  mode: Mode;
 
-  @Column('date', { nullable: false })
+  @Column({ type: 'enum', enum: isHidden, nullable: false })
+  hidden: isHidden;
+
+  @Column({ type: 'enum', enum: Status, default: Status.onProgress, nullable: false })
+  status: Status;
+
+  @Column({ type: 'varchar', nullable: false })
   startDate: string;
 
-  @Column('date', { nullable: true })
-  endDate: string;
-
-  @Column({ type: 'enum', name: 'hidden', enum: isHidden })
-  hidden!: isHidden;
-
-  @Column({ type: 'enum', name: 'status', enum: Status, default: Status.onProgress })
-  status!: Status;
-
-  @Column('timestamp', { nullable: false, default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date;
-
-  @Column('timestamp', { nullable: false, default: () => 'CURRENT_TIMESTAMP' })
-  updatedAt: Date;
+  @Column({ type: 'varchar', nullable: true })
+  endDate: string | null;
 
   @ManyToOne(() => User, (user) => user.quests, {
     onDelete: 'CASCADE',
-    onUpdate: 'CASCADE',
   })
   user: User;
 
   @OneToMany(() => SideQuest, (sideQuest) => sideQuest.quest)
   sideQuests: SideQuest[];
+
+  static create(
+    userId: number,
+    title: string,
+    difficulty: Difficulty,
+    mode: Mode,
+    hidden: isHidden,
+    startDate: string,
+    endDate: string | null
+  ): Quest {
+    const quest = new Quest();
+    quest.userId = userId;
+    quest.title = title;
+    quest.difficulty = difficulty;
+    quest.mode = mode;
+    quest.hidden = hidden;
+    quest.startDate = startDate;
+    quest.endDate = endDate;
+
+    return quest;
+  }
 }
