b9d53a85f9084a941a0d18b0f08dd84b25c59532
namu56 <swi920224@gmail.com>
2024-07-01 01:35:36 +0900

    chore: request의 payload 타입을 인터페이스에서 클래스로 수정

--- server/src/modules/auth/controllers/auth.controller.ts
+++ server/src/modules/auth/controllers/auth.controller.ts
@@ -23,8 +23,9 @@ import {
 import { CurrentUser } from 'src/core/decorators/current-user.decorator';
 import { LocalAuthGuard } from '../../../core/guards/local-auth.guard';
 import { JwtAuthGuard } from '../../../core/guards/jwt-auth.guard';
-import { AccessTokenPayload, AuthTokens } from '../auth.interface';
 import { AuthTokenInterceptor } from 'src/core/interceptors/auth-token.interceptor';
+import { AccessTokenPayload } from 'src/common/dto/token';
+import { AuthTokenResponse } from 'src/common/responses/token';
 
 @Controller('auth')
 @ApiTags('Auth API')
@@ -38,7 +39,7 @@ export class AuthController {
   @ApiOperation({ summary: '로그인' })
   @ApiOkResponse({ description: '로그인 성공 시 토큰 반환' })
   @ApiNotFoundResponse({ description: 'fail- User not found' })
-  async login(@CurrentUser() user: AccessTokenPayload): Promise<AuthTokens> {
+  async login(@CurrentUser() user: AccessTokenPayload): Promise<AuthTokenResponse> {
     return await this.authService.login(user);
   }
 
@@ -49,7 +50,7 @@ export class AuthController {
   @ApiOkResponse({ description: '로그아웃 성공 시 쿠키의 토큰 삭제' })
   @ApiUnauthorizedResponse({ description: 'Unauthenticated' })
   @ApiForbiddenResponse({ description: 'fail - Invaild token' })
-  async logout(@CurrentUser() user: AccessTokenPayload, @Res() res: Response) {
+  async logout(@CurrentUser() user: AccessTokenPayload, @Res() res: Response): Promise<void> {
     await this.authService.logout(user.id);
     res.clearCookie('refreshToken');
     res.sendStatus(HttpStatus.NO_CONTENT);
@@ -58,7 +59,7 @@ export class AuthController {
   @Post('token')
   @UseInterceptors(AuthTokenInterceptor)
   @HttpCode(HttpStatus.OK)
-  async refreshToken(@Req() req: Request): Promise<AuthTokens> {
+  async refreshToken(@Req() req: Request): Promise<AuthTokenResponse> {
     const refreshToken: string = req.cookies.refreshToken;
     if (!refreshToken) {
       throw new HttpException('Refresh token not found', HttpStatus.UNAUTHORIZED);
