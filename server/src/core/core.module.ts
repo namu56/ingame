ddeb3b6ae0125adc1ef641320e3109746e2cff62
namu56 <swi920224@gmail.com>
2024-07-01 19:36:09 +0900

    fix: 순환 의존성 문제 해결

--- server/src/core/core.module.ts
+++ server/src/core/core.module.ts
@@ -5,13 +5,28 @@ import { redisProviders } from './database/redis/redis.provider';
 import { WinstonLoggerMiddleware } from './middlewares/winston-logger.middleware';
 import { CustomTypeOrmModule } from './database/typeorm/typeorm.module';
 import { TokenModule } from './token/token.module';
+import { GoogleStrategy, JwtStrategy, LocalStrategy } from './strategies';
+import { PassportModule } from '@nestjs/passport';
+import { AuthModule } from 'src/modules/auth/auth.module';
+import { UserModule } from 'src/modules/user/user.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
-const modules = [CustomTypeOrmModule, TokenModule];
+const modules = [
+  CustomTypeOrmModule,
+  TokenModule,
+  PassportModule,
+  AuthModule,
+  UserModule,
+  SchedulerModule,
+  ScheduleModule.forRoot(),
+];
+const strategies = [JwtStrategy, LocalStrategy, GoogleStrategy];
 
 @Global()
 @Module({
   imports: [...modules],
-  providers: [LevelCalculatorService, WinstonLoggerService, ...redisProviders],
+  providers: [LevelCalculatorService, WinstonLoggerService, ...redisProviders, ...strategies],
   exports: [LevelCalculatorService, WinstonLoggerService, ...modules, ...redisProviders],
 })
 export class CoreModule implements NestModule {
--- server/src/core/scheduler/scheduler.module.ts
+++ server/src/core/scheduler/scheduler.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quest } from '../../entities/quest/quest.entity';
 import { SchedulerService } from './scheduler.service';
-import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerController } from './scheduler.controller';
 import { PointModule } from '../../modules/point/point.module';
 import { SideQuest } from '../../entities/side-quest/side-quest.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quest, SideQuest]), ScheduleModule.forRoot(), PointModule],
+  imports: [TypeOrmModule.forFeature([Quest, SideQuest]), PointModule],
   providers: [SchedulerService],
   controllers: [SchedulerController],
   exports: [SchedulerService],
