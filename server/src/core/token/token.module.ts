4662975c5d5681304353583847d9d4702c04bb22
namu56 <swi920224@gmail.com>
2024-07-03 00:30:41 +0900

    chore: config 폴더 core 안으로 이동

--- server/src/core/token/token.module.ts
+++ server/src/core/token/token.module.ts
@@ -1,10 +1,10 @@
 import { Module, Provider } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtConfig } from 'src/configs/jwt.config';
 import { TOKEN_SERVICE_KEY } from './interfaces/token-service.interface';
 import { TokenService } from './token.service';
 import { RefreshTokenRepositoryModule } from 'src/entities/refresh-token/refresh-token-repository.module';
+import { JwtConfig } from '../config';
 
 const tokenService: Provider = {
   provide: TOKEN_SERVICE_KEY,
